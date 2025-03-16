d56094f71cb84be824d3ee57f8f560031f8e3b56
NikitaGhimire <nikitaghimire1@gmail.com>
2025-03-16 03:14:07 -0700

    render base url update

--- frontend/src/context/config.js
+++ frontend/src/context/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://your-actual-render-backend-url.onrender.com'
+    ? 'https://wheres-waldo-22nc.onrender.com'
     : 'http://localhost:5001';
