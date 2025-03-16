e69932c833f0509ae44e8864524f8043a5f95981
NikitaGhimire <nikitaghimire1@gmail.com>
2025-03-16 02:28:44 -0700

    Add proptected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import DashboardPage from "./components/pages/Dashboard";
 import Footer from "./components/pages/Footer";
 import NavBar from "./components/pages/NavBar";
 import UploadPage from './components/pages/UploadPage';
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -18,8 +19,22 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/loginPage" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/upload" element={<UploadPage />} />
+          <Route 
+            path="/dashboard" 
+            element={
+              <ProtectedRoute role="author">
+                <DashboardPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/upload" 
+            element={
+              <ProtectedRoute role="author">
+                <UploadPage />
+              </ProtectedRoute>
+            } 
+          />
         </Routes>
         <Footer />
       </Router>
